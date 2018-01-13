0608b1e0a519b80ba17af3b97143f201087b995c
Zed <clement.francois@epitech.eu>
2018-01-13 01:14:37 +0100

    ChainCollider: ADD: Counter for the score

--- BubbleStruggle/Assets/Scripts/ChainCollider.cs
+++ BubbleStruggle/Assets/Scripts/ChainCollider.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 
 public class ChainCollider : MonoBehaviour {
+
+    private int m_score;
+    [SerializeField] private Text m_scoreText;
+
+    private void SetScore() {
+        m_scoreText.text = "SCORE: " + m_score.ToString();
+    }
+
+    void Start() {
+        m_score = 0;
+        SetScore();
+    }
 
     void OnTriggerEnter2D(Collider2D colliderHitten) {
         Chain_physics.m_isFireing = false;
 
         if (colliderHitten.tag == "Ball") {
+            m_score += 10;
             colliderHitten.GetComponent<Ball>().Split();
+            Debug.Log("Destroying ball");
+            SetScore();
         }
 
     }
